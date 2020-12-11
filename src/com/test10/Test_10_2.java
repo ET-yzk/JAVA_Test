a9d88607c5c347764bb57a658f782a67e04df92c
E T <1581790391@qq.com>
2020-12-11 16:03:17 +0800

    code update

--- src/com/test10/Test_10_2.java
+++ src/com/test10/Test_10_2.java
@@ -1,4 +1,32 @@
 package com.test10;
 
+import java.io.File;
+
+class FileInfo {
+    File f1 = new File("src/com/test10/Test_10_Data/倚天屠龙记.txt");
+    File f2 = new File("src/com/test10/Test_10_Data/笑傲江湖.txt");
+    File f3 = new File("src/com/test10/Test_10_Data/鹿鼎记.txt");
+}
+
+// 倒叙索引
+class InvertedIndex {
+    File file;
+    InvertedIndex(File file) {
+        this.file = file;
+    }
+}
+
+class IndexRunnable implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
+
 public class Test_10_2 {
+    public static void main(String[] args) {
+
+    }
+
 }
