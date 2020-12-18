82a6a0ca07293f73896466a47963b0accbd6068b
E T <1581790391@qq.com>
2020-12-18 16:50:12 +0800

    code update

--- src/com/test11/Test_11_2.java
+++ src/com/test11/Test_11_2.java
@@ -5,16 +5,29 @@
 
 package com.test11;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Test_11_2 {
     public static void main(String[] args) {
+        System.out.println("请输入一个数:");
         Scanner sc = new Scanner(System.in);
-        HashMap<String,Integer> hs = new HashMap<>();
+        String num = sc.next();
 
-        while (sc.hasNext()) {
-            hs.merge()
+        HashMap<String,Integer> hs = new HashMap<>();
+        for (int i = 0; i < num.length(); i ++) {
+            hs.merge(String.valueOf(num.charAt(i)), 1, Integer::sum);
+        }
+
+        ArrayList<Map.Entry<String, Integer>> countList = new ArrayList<>(hs.entrySet());
+        countList.sort(new Comparator<>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        });
+
+        for (Map.Entry<String, Integer> i: countList) {
+            System.out.println(i.getKey() + ":" + i.getValue() + "次");
         }
     }
 }
