99a64822a3fd58e254333706091c5d623019dfc7
E T <1581790391@qq.com>
2020-12-18 16:02:55 +0800

    code update

--- src/com/test11/Test_11_2.java
+++ src/com/test11/Test_11_2.java
@@ -1,4 +1,20 @@
+/* 2019期末考试
+* 输入一个100000位的数字，
+* 请计算这个数中各位数出现的次数。
+* 如输入1223，则各位数出现的次数为1：1次；2：2次；3：1次。*/
+
 package com.test11;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Test_11_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<String,Integer> hs = new HashMap<>();
+
+        while (sc.hasNext()) {
+            hs.merge()
+        }
+    }
 }
