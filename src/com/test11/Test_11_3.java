9d7d1b247f7a0b928f6090623da6c9ca9d3bcc8c
E T <1581790391@qq.com>
2020-12-18 18:44:20 +0800

    code update

--- src/com/test11/Test_11_3.java
+++ src/com/test11/Test_11_3.java
@@ -16,7 +16,57 @@ public class Test_11_3 {
                 System.out.println(str[i] + str[j] + str[j] + str[i]);
             }
         }
+        findUnHuiNum();
     }
+
+    public static void findUnHuiNum() {
+        // 生成回文数:它与自己的翻转数相加,200以内的数字中，都可以在30步以内变成回文数，
+        //除了一个数字很特殊，就算迭代了1000次，它还是顽固地拒绝回文，请找出该数。
+        for(int i=1;i<=200;i++)
+        {
+            int j=1;
+            //long保证数字不超出范围
+            long sum=i;
+            while(j<=30)
+            {
+                //转字符串类型便于循环
+                String str = String.valueOf(sum);
+                boolean flag=false;
+                for(int a = 0, b = str.length()-1; a < str.length(); a++, b--)
+                {
+                    //判断对应位是否相等
+                    if(str.charAt(a)!=str.charAt(b))
+                    {
+                        //不是回文数
+                        flag =true;
+                    }
+                }
+                if(flag)
+                {
+                    //反转
+                    long k = sum, tmp = 0;
+                    while (k != 0) {
+                        tmp = tmp * 10 + k % 10;
+                        k/=10;
+                    }
+                    //反转之和
+                    sum += tmp;
+                }
+                else
+                {
+                    //是回文数
+                    break;
+                }
+                j++;
+            }
+            if(j>30)
+            {
+                //输出循环30次还不是回文数
+                System.out.println(i);
+            }
+        }
+    }
+
 }
 
 //实现类似python字符串的乘法：a * 2 -> aa
